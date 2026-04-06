0134dd7b9d697759c2046e40547e02d331dddaca
autodl-vllm-qwen3 <2315516642@qq.com>
2026-04-07 01:03:49 +0800

    finish creating api application and test

--- .ipynb_checkpoints/model_download-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from modelscope import snapshot_download
-
-model_dir = snapshot_download(
-    'Qwen/Qwen3-0.6B',
-    cache_dir='/root/autodl-tmp',
-    revision='master'
-)
-
-print(f"模型已下载到{model_dir}")
