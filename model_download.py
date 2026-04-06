d853a96baea0110d3393742ad4b09bf3fd562bad
autodl-vllm-qwen3 <2315516642@qq.com>
2026-04-06 17:51:41 +0800

    first commit

---
+++ model_download.py
@@ -0,0 +1,9 @@
+from modelscope import snapshot_download
+
+model_dir = snapshot_download(
+    'Qwen/Qwen3-8B',
+    cache_dir='/root/autodl-tmp',
+    revision='master'
+)
+
+print(f"模型已下载到{model_dir}")
